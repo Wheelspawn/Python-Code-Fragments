1673016ec528339391029df7dc6547a10be437ab
Nathaniel Le Sage <nataplane@gmail.com>
2017-03-10 12:17:40 -0600

    Update ChineseVocabTester.py

--- ChineseVocabTester.py
+++ ChineseVocabTester.py
@@ -1,12 +1,21 @@
 import random
 
-words = { 'good; fine': ['hǎo'],
-          'you': ['nǐ'],
-          'How are you?': ['nǐ hǎo ma?'],
-          'I': ['wǒ'],
-          'Very well': ['hěn hǎo'],
-          'I am fine.': ['wǒ hěn hǎo'],
-          'And you?': ['nǐ ne?'] }
+words = { 'good; fine':  ['hǎo'],
+          'you':  ['nǐ'],
+          'How are you?':  ['nǐ hǎo ma?'],
+          'I':  ['wǒ'],
+          'Very well':  ['hěn hǎo'],
+          'I am fine.':  ['wǒ hěn hǎo'],
+          'And you?':  ['nǐ ne?'],
+          'Not bad':  [''],
+          'question particle':  ['ma?'],
+          'thanks': ['xièxie'],
+          'you are welcome': ['bù kèqì'],
+          'excuse me; I am sorry': ['duìbùqǐ'],
+          'It is nothing; do not worry; no problem': ['méi shì'],
+          'goodbye': ['zàijiàn']
+          }
+
 
 global word
 word = random.sample(list(words),1)[0]
@@ -17,4 +26,4 @@ def test(): # sample random word
     print(word)
 
 def check(): # display pinyin word
-    print(words[word])
+    print(words[word][0])
